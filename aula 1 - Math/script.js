ae49f319792664168b86234a52ff2979071941eb
Sheep8684 <arthurtutu2803@gmail.com>
2024-09-02 08:09:44 -0300

    arredondamento com Math

---
+++ aula 1 - Math/script.js
@@ -0,0 +1,15 @@
+//arredondamento
+
+let numero = 4.7
+
+//Math.round é um arredondamento comum
+
+console.log(Math.round(numero))
+
+//math.ceil é um arredondamento para cima
+
+console.log(Math.ceil(numero))
+
+//math.floor é um arredondamento para baixo
+
+console.log(Math.floor(numero))
