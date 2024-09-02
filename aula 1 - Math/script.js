d18f273a74ec3dd2291e2db8ace77c491230ffcf
Sheep8684 <arthurtutu2803@gmail.com>
2024-09-02 08:17:41 -0300

    operações matemáticas

--- aula 1 - Math/script.js
+++ aula 1 - Math/script.js
@@ -13,3 +13,23 @@ console.log(Math.ceil(numero))
 //math.floor é um arredondamento para baixo
 
 console.log(Math.floor(numero))
+
+//math.pow gera potenciação
+
+console.log(Math.pow(2, 3))
+
+//Math.sqrt gera radiciação
+
+console.log(Math.sqrt(35))
+
+//Math.abs transforma tudo em positivo
+
+console.log(Math.abs(-5))
+
+//Math.min seleciona o menor número de uma sequência
+
+console.log(Math.min(43, 25, 76, 10))
+
+//math.max seleciona o maior número de uma sequência
+
+console.log(Math.max(75, 22, 37, 89))
